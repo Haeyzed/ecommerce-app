fa2ddabfffcd061f5822bb158c6bbafe54b7ebd5
Azeez Muibi <amuibi@softalliance.com>
2026-03-13 04:42:33 +0100

    Update

--- components/image-zoom.tsx
+++ components/image-zoom.tsx
@@ -8,22 +8,25 @@ import { ImageZoom } from '@/components/ui/image-zoom'
 function ImageZoomCell({ src, alt }: { src: string; alt: string }) {
   const { resolvedTheme } = useTheme()
   return (
-    <ImageZoom
-      backdropClassName={cn(
-        resolvedTheme === 'dark'
-          ? '[&_[data-rmiz-modal-overlay="visible"]]:bg-white/80'
-          : '[&_[data-rmiz-modal-overlay="visible"]]:bg-black/80'
-      )}
-    >
-      <Image
-        src={src}
-        alt={alt}
-        width={40}
-        height={40}
-        className='size-10 rounded-md object-cover'
-        unoptimized
-      />
-    </ImageZoom>
+    <div className="size-10 shrink-0 overflow-hidden rounded-md">
+      <ImageZoom
+        backdropClassName={cn(
+          resolvedTheme === 'dark'
+            ? '[&_[data-rmiz-modal-overlay="visible"]]:bg-white/80'
+            : '[&_[data-rmiz-modal-overlay="visible"]]:bg-black/80'
+        )}
+        className="block size-full"
+      >
+        <Image
+          src={src}
+          alt={alt}
+          width={40}
+          height={40}
+          className="size-10 min-h-10 min-w-10 rounded-md object-cover"
+          unoptimized
+        />
+      </ImageZoom>
+    </div>
   )
 }
 
